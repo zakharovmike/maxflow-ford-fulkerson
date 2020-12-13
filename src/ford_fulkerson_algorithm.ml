390e828c0413441b13f2af1034ec9b0f0d5ee4a0
Mikhail Zakharov <mizak@envisage.no>
2020-12-13 02:17:58 +0100

    Add comments and minor refactor in ford_fulkerson_algorithm

--- src/ford_fulkerson_algorithm.ml
+++ src/ford_fulkerson_algorithm.ml
@@ -12,46 +12,70 @@ let rec print_path = function
     print_path rest
   | [] -> Printf.printf "\n%!"
 
+(* Uses depth-first search to find a path in a graph 
+ * from the source to the sink while avoiding forbidden_nodes. *)
 let find_path graph forbidden_nodes source sink =
+
   let rec dfs visited node = 
     if node = sink then Some empty_path else
-    let neighbors = List.filter 
+
+    (* Find a node's adjacent nodes, avoiding:
+     *  - those that have already been visited, and
+     *  - those that are already saturated *)
+    let neighbors = List.filter
       (fun (node, lbl) -> not (List.mem node visited) && lbl != 0) 
       (out_arcs graph node)
     in
     match neighbors with
     | [] -> None
-    | (next, _) :: rest ->
-      match dfs (next::visited) next with
-      | None -> dfs (next::visited) node
-      | Some path -> Some (next :: path)
+    | (next, _) :: _ ->
+      match dfs (next::visited) next with   (* Search for path from an adjacent node *)
+      | None -> dfs (next::visited) node    (* If no path found, retry from parent node, avoiding that adjacent node as well *)
+      | Some path -> Some (next :: path)    (* If a path has been found, contruct it on its way back up the call stack *)
   in
+
   match dfs forbidden_nodes source with
-  | Some path -> Some (source :: path)
+  | Some path -> Some (source :: path)      (* If a path is found, add the source node to it and return  *)
   | None -> None
 
 let find_bottleneck path graph =
+
   let rec loop path graph bottleneck =
     match path with
     | a :: b :: rest -> 
-      min 
-      (loop (b::rest) graph (abs (Option.get (find_arc graph a b)))) 
-      bottleneck
+      let capacity = Option.get (find_arc graph a b) in   (* The arc existes for sure given it's in a path *)
+      min (loop (b::rest) graph capacity) bottleneck      (* min capacity bottleneck, recursively on all arcs of path *)
     | last :: rest -> bottleneck
     | [] -> bottleneck
   in
+
   loop path graph Int.max_int
 
-let update_residual_graph graph path bottleneck =
-  let rec loop graph path bottleneck = 
+(* The residual graph contains all forward and backward arcs after
+ * the flow has been added along the path.
+ * 
+ * A forward arc is an original arc of the graph with its remaining capacity.
+ * Instead of starting at 0 and adding flow up to at most its maximum capacity,
+ * it starts at its maximum capacity and flow is subtracted, leaving the remaining capacity.
+ * When remaining capacity is at 0 the arc is saturated and cannot be used.
+ *
+ * A backward arc (B->A) is the opposite of a forward arc (A->B), representing how much flow
+ * is already going from A to B and thus how much flow can be removed from A->B.
+ * All backward arcs start at 0 (non-existent) and flow is added to them.
+ * If used in a path, it acts like a forward arc and its flow is subtracted.
+ *)
+let update_residual_graph graph path flow =
+
+  let rec loop graph path flow = 
     match path with
     | a :: b :: rest ->
-      add_arc (loop graph (b::rest) bottleneck) a b (-bottleneck)
+      add_arc (loop graph (b::rest) flow) a b (-flow)   (* Subtract flow from every arc along the path, leaving their remaining capacity *)
     | last :: [] -> graph
     | [] -> graph
   in
-  let forward_updated_graph = loop graph path bottleneck in
-  let backward_updated_graph = loop forward_updated_graph (List.rev path) (-bottleneck) in
+
+  let forward_updated_graph = loop graph path flow in                                 (* Subtract flow from forward arcs *)
+  let backward_updated_graph = loop forward_updated_graph (List.rev path) (-flow) in  (* Add flow to their opposite backward arcs *)
   backward_updated_graph
   
 
@@ -59,6 +83,7 @@ let update_residual_graph graph path bottleneck =
  * on the given graph using the Ford-Fulkerson algorithm.
 *)
 let get_max_flow graph source sink =
+
    let rec while_path_exists residual_graph flow =
     match (find_path residual_graph [] source sink) with
     | Some path -> 
@@ -67,5 +92,6 @@ let get_max_flow graph source sink =
       while_path_exists updated_residual_graph (flow + bottleneck)
     | None -> flow
    in
+
    let max_flow = while_path_exists graph 0 in
    max_flow
--- src/ford_fulkerson_algorithm.mli
+++ src/ford_fulkerson_algorithm.mli
@@ -3,9 +3,11 @@ open Graph
 (* A path is a list of nodes. *)
 type path
 
-(* The empty graph. *)
+(* The empty path. *)
 val empty_path: path
 
+(* Prints path
+ * ex: 0 2 3 5 7 *)
 val print_path: path -> unit
 
 (* find_path gr forbidden id1 id2 
@@ -16,11 +18,14 @@ val print_path: path -> unit
 *)
 val find_path: int graph -> id list -> id -> id -> path option
 
+(* find_bottleneck path graph
+ *   returns the minimum capacity along the path in graph *)
 val find_bottleneck: path -> int graph -> int
 
+(* update_residual_graph graph path flow
+ *   returns a graph where the flow has been added along the path in graph *)
 val update_residual_graph: int graph -> path -> int -> int graph
 
 (* get_max_flow graph source sink
- * returns the max flow between the source and the sink on the given graph.
-*)
+ *   returns the max flow between the source and the sink on the given graph *)
 val get_max_flow: int graph -> id -> id -> int
